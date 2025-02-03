1105ec1490d074c6e88088539c172048b2b27feb
Moin Almin <almin.crypt@gmail.com>
2025-02-03 22:48:57 +0530

    fix: db message fix

--- src/config/db.js
+++ src/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('✅ MongoDB Connected...');
+        console.log('MongoDB Connected...');
     } catch (error) {
-        console.error('❌ MongoDB Connection Error:', error);
+        console.error('MongoDB Connection Error:', error);
         process.exit(1);
     }
 };
